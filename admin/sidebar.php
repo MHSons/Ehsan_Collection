22d2d043b5cde56056f032e9a90d23f6bda6c82f
MHSons <muhammadasadhanif786@gmail.com>
2025-11-26 17:04:15 +0500

    Delete admin directory

--- admin/sidebar.php
+++
@@ -1,9 +0,0 @@
-<!-- File: admin/sidebar.php -->
-<div id="admin-sidebar" dir="ltr">
-    <h2 class="logo-text">MAH Admin</h2>
-    <nav>
-        <ul>
-            <li class="<?= (basename($_SERVER['PHP_SELF']) == 'dashboard.php') ? 'active' : '' ?>">
-                <a href="dashboard.php">Dashboard</a>
-            </li>
-            <li class="<?= (basename($_SERVER['PHP_SELF']) == 'orders.php') ? 'active'_
